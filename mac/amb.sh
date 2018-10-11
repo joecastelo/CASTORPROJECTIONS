e999e7c7f4401c382a29ca9cad3120740eded085
Joao Henrique Castelo <joaohenrique@difme-daniel3.ird.gov.br>
2018-10-11 12:04:31 -0300

    Display

---
+++ mac/amb.sh
@@ -0,0 +1,6 @@
+export PATH=$PATH:/home/programs/gate/gate_v8.1/gate_v8.1-install/bin
+export GC_GATE_EXE_DIR=/home/programs/gate/gate_v8.1/gate_v8.1-install/bin
+export GC_DOT_GATE_DIR=/home/programs/gate/gate_v8.1/
+source /home/programs/geant4/geant4.10.03.p02/geant4.10.03.p02-install/bin/geant4.sh
+source /home/programs/root/root_v6.10.04/root-6.10.04-build/bin/thisroot.sh
+export PATH=$PATH:/home/programs/castor/castor_v2.0.1
---
+++ mac/runshell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -x
+nohup Gate -a [i,1][act,325000][source,131IGamas] main.mac > flowlogi1.txt &
+nohup Gate -a [i,2][act,325000][source,131IGamas] main.mac > flowlogi2.txt &
+nohup Gate -a [i,3][act,325000][source,131IGamas] main.mac > flowlogi3.txt &
+nohup Gate -a [i,4][act,325000][source,131IGamas] main.mac > flowlogi4.txt &
+nohup Gate -a [i,5][act,325000][source,131IGamas] main.mac > flowlogi5.txt &
+nohup Gate -a [i,6][act,325000][source,131IGamas] main.mac > flowlogi6.txt &
+nohup Gate -a [i,7][act,325000][source,131IGamas] main.mac > flowlogi7.txt &
+nohup Gate -a [i,8][act,325000][source,131IGamas] main.mac > flowlogi8.txt &
+nohup Gate -a [i,9][act,325000][source,131IGamas] main.mac > flowlogi9.txt &
+nohup Gate -a [i,10][act,325000][source,131IGamas] main.mac > flowlogi10.txt &
+nohup Gate -a [i,11][act,325000][source,131IGamas] main.mac > flowlogi11.txt &
+nohup Gate -a [i,12][act,325000][source,131IGamas] main.mac > flowlogi12.txt &
+
+
+
